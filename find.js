dc7aac0ba74ab5ac1c628599ec597ebe58ad4681
mallocaholic <jrmarcondesb@gmail.com>
2023-07-20 16:10:57 -0300

    prettifier document

--- find.js
+++ find.js
@@ -1,5 +1,5 @@
-class Find{
-    constructor(){
+class Find {
+    constructor() {
         this.cells = [];
         this.start = null;
         this.goal = null;
@@ -14,19 +14,19 @@ class Find{
         this.time = 0;
     }
 
-    drawAndAnimate(colorFrontier, colorNotExplored, colorPath, animationSpeed){
+    drawAndAnimate(colorFrontier, colorNotExplored, colorPath, animationSpeed) {
         rectMode(CENTER);
         // console.log(this.cellSize);
 
         let size = this.cells[0][0].size;
 
-        if(this.exploredHistory.length > 0 && this.frontierHistory.length > 0){
-            this.time = this.time + (deltaTime/animationSpeed);
+        if (this.exploredHistory.length > 0 && this.frontierHistory.length > 0) {
+            this.time = this.time + (deltaTime / animationSpeed);
         } else {
             this.showPath(colorPath);
         }
 
-        if(this.time >= 100 && this.exploredHistory.length > 0 && this.frontierHistory.length > 0){
+        if (this.time >= 100 && this.exploredHistory.length > 0 && this.frontierHistory.length > 0) {
             this.currentExplored = this.exploredHistory.shift();
             this.currentFrontier = this.frontierHistory.shift();
         }
@@ -36,73 +36,73 @@ class Find{
         // fill(colorExplored);
         noStroke();
         fill(colorNotExplored);
-        for(let i = 0; i < this.cells.length; i++){
-            for(let j = 0; j < this.cells[i].length; j++){
+        for (let i = 0; i < this.cells.length; i++) {
+            for (let j = 0; j < this.cells[i].length; j++) {
                 let draw = true;
 
-                for(let k = 0; k < this.currentExplored.length; k++){
-                    if((this.currentExplored[k].i == i && this.currentExplored[k].j == j)){
+                for (let k = 0; k < this.currentExplored.length; k++) {
+                    if ((this.currentExplored[k].i == i && this.currentExplored[k].j == j)) {
                         draw = false;
                         break;
                     }
                 }
 
-                if(draw) rect(this.cells[i][j].x + size/2, this.cells[i][j].y + size/2, size, size);
+                if (draw) rect(this.cells[i][j].x + size / 2, this.cells[i][j].y + size / 2, size, size);
             }
         }
 
         fill(colorFrontier);
-        for(let i = 0; i < this.currentFrontier.length; i++){
+        for (let i = 0; i < this.currentFrontier.length; i++) {
             noStroke();
-            rect(this.currentFrontier[i].x + size/2, this.currentFrontier[i].y + size/2, size, size);
+            rect(this.currentFrontier[i].x + size / 2, this.currentFrontier[i].y + size / 2, size, size);
         }
 
         rectMode(CORNER);
     }
 
-    showPath(colorPath){
-        for(let i = 0; i < this.path.length; i++){
+    showPath(colorPath) {
+        for (let i = 0; i < this.path.length; i++) {
             fill("#ff0000d7")
             noStroke();
-            rect(this.path[i].x + this.cells[0][0].size/2, this.path[i].y + this.cells[0][0].size/2, this.cells[0][0].size, this.cells[0][0].size);
+            rect(this.path[i].x + this.cells[0][0].size / 2, this.path[i].y + this.cells[0][0].size / 2, this.cells[0][0].size, this.cells[0][0].size);
         }
 
     }
 
 
-    bfs(cells, start, goal){
+    bfs(cells, start, goal) {
         this.cells = cells;
         this.start = cells[start.i][start.j];
         this.goal = cells[goal.i][goal.j];
 
-        let frontier =  [];
-        let explored =  [];
+        let frontier = [];
+        let explored = [];
         let cameFrom = {};
 
         frontier.push(this.start);
         explored.push(this.start);
         cameFrom[this.start.i + "," + this.start.j] = null;
 
-        while(frontier.length > 0 && !frontier.includes(this.goal) && !explored.includes(this.goal)){
+        while (frontier.length > 0 && !frontier.includes(this.goal) && !explored.includes(this.goal)) {
             this.exploredHistory.push(explored.slice());
             let current = frontier.shift();
             let neighbors = current.neighbors;
 
-            for(let i = 0; i < neighbors.length; i++){
+            for (let i = 0; i < neighbors.length; i++) {
                 let neighbor = neighbors[i];
 
-                if(!explored.includes(neighbor)){
+                if (!explored.includes(neighbor)) {
                     frontier.push(neighbor);
                     explored.push(neighbor);
                     cameFrom[neighbor.i + "," + neighbor.j] = current;
                 }
-           }
-        this.frontierHistory.push(frontier.slice());
+            }
+            this.frontierHistory.push(frontier.slice());
         }
 
         let path = [];
         let current = this.goal;
-        while(current != null){
+        while (current != null) {
             path.unshift(current);
             current = cameFrom[current.i + "," + current.j];
         }
